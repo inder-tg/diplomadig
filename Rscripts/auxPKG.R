0665cbd5a26b35eb21367bb2716891935d154abe
Inder Tecuapetla <inder.tg@gmail.com>
2023-07-28 12:56:51 -0600

    Needed packages

--- Rscripts/auxPKG.R
+++ Rscripts/auxPKG.R
@@ -2,15 +2,18 @@
 #
 # Elaborado por Inder Tecuapetla, May 31, 2023
 #
+# Modificado Julio 28, 2023
+#
 # Funciones auxiliares
 # 
 # Hecho para SELPER/CEOS Working Group Chapter D Training Group
 #
 # -----------------------------------------------------------------------------
 
-neededPackages <- c("raster", "mapview", "geoTS", "RColorBrewer",
-                    "rasterVis", "ggplot2", "dplyr", "gtools",
-                    "bfast")
+neededPackages <- c("raster", "mapview", "RColorBrewer", "gtools",
+                    "foreach", "doParallel", "Kendall", "trend",
+                    "geoTS", "sta", "rasterVis", "bfast", "tmap", 
+                    "ggplot2", "dplyr")
 
 packagesToInstall <- neededPackages[!(neededPackages %in% installed.packages()[,"Package"])]
 
